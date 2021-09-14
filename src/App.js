92f53056f086563f7b6dd36606b5629fa9d45f6e
LeeKyuchan <time2209@naver.com>
2021-09-14 16:47:50 +0900

    Customer.js 정의 / map 함수를 이용하여 배열 출력

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+
+const customers = [{
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name':'홍길동1',
+  'birthday': '961222',
+  'gender':'남자',
+  'job':'대학생'
+},
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길동2',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '홍길동3',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  }];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop mamnagement system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
     </div>
   );
 }
