555a37d47a8aa85eacffba698ffda35a87aadea9
LeeKyuchan <time2209@naver.com>
2021-09-15 14:58:22 +0900

    Customers REST API 모듈 추가

--- client/src/App.js
+++ client/src/App.js
@@ -22,33 +22,25 @@ const styles = theme => ({
   }
 })
 
-
-const customers = [{
-  'id':1,
-  'image':'https://placeimg.com/64/64/1',
-  'name':'홍길동1',
-  'birthday': '961222',
-  'gender':'남자',
-  'job':'대학생'
-},
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '홍길동2',
-    'birthday': '961222',
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '홍길동3',
-    'birthday': '961222',
-    'gender': '남자',
-    'job': '대학생'
-  }];
-
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+  
+
   render(){
     const { classes } = this.props;
     return (
@@ -66,7 +58,7 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {
-              customers.map(c => {
+              this.state.customers ? this.state.customers.map(c => {
                 return (
                   <Customer
                     key={c.id}
@@ -78,7 +70,7 @@ class App extends Component {
                     job={c.job}
                   />
                 )
-              })
+              }) :  ""
             }
           </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: "Hello Express!"});
+app.get('/api/customers', (req, res) => {
+    res.send([
+            {
+                'id':1,
+                'image':'https://placeimg.com/64/64/1',
+                'name':'홍길동1',
+                'birthday': '961222',
+                'gender':'남자',
+                'job':'대학생'
+            },
+            {
+                'id': 2,
+                'image': 'https://placeimg.com/64/64/2',
+                'name': '홍길동2',
+                'birthday': '961222',
+                'gender': '남자',
+                'job': '대학생'
+            },
+            {
+                'id': 3,
+                'image': 'https://placeimg.com/64/64/3',
+                'name': '홍길동3',
+                'birthday': '961222',
+                'gender': '남자',
+                'job': '대학생'
+            }
+        ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
