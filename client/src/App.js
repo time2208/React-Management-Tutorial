683b8b372f37c819c7d368c09ccb22b6d1071b86
LeeKyuchan <time2209@naver.com>
2021-09-16 12:24:12 +0900

    Customer 데이터베이스 추가 및 nodejs 데이터베이스 연결

--- client/src/App.js
+++ client/src/App.js
@@ -35,9 +35,9 @@ class App extends Component {
 
   componentDidMount(){
     this.timer = setInterval(this.progress, 20);
-    // this.callApi()
-    //   .then(res => this.setState({customers: res}))
-    //   .catch(err => console.log(err));
+    this.callApi()
+       .then(res => this.setState({customers: res}))
+       .catch(err => console.log(err));
   }
 
   callApi = async () => {
